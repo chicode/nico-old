33bea1b1bae16b462d18e3f8e7dc75f5b1059a58
Anton <antonoutkine@gmail.com>
2018-09-08 14:16:25 -0500

    Improve greeting text

--- src/nico/components/Greeting.vue
+++ src/nico/components/Greeting.vue
@@ -10,7 +10,7 @@
       For starters, try drawing a sprite in the first grid square and getting it to display on the screen. Hint: the editor tab has a useful booklet that will tell you all about the different functions you can use to do stuff like that.
     </p>
     <p>
-      So go, make the games of your dreams! And if you ever decide to come back, you don't have to worry about anything - nico saves everything automatically (as long as you don't clear your cache or go into incognito mode).
+      After that, the possibilities really are endless - you can think of nico as that <span class="accent-3">secret</span>&nbsp;<span class="accent-1">superpower</span> that you always thought you had. And if you ever decide to come back, you don't have to worry about anything - nico saves everything automatically (as long as you don't clear your cache or go into incognito mode).
     </p>
   </div>
 </template>
